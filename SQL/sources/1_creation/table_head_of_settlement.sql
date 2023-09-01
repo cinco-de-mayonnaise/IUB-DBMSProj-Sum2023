e99ec701d249cb077f2a2d956b31ea177495000b
joysaha007 <126173800+joysaha007@users.noreply.github.com>
2023-09-02 02:06:02 +0600

    add 1 table

---
+++ SQL/sources/1_creation/table_head_of_settlement.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Head_of_Settlement(
+	HoSemployeeID INT PRIMARY KEY, -- constraint Foreign Key Employee_Table
+        finalApprovalStatus VARCHAR(300),
+        finalApprovalDate DATE
+
+);
