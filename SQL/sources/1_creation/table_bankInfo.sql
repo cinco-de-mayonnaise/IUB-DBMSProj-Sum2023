3fc454155bab630f53f79fbbf267ac00584b1e2f
AbdullahTrees <124304231+cinco-de-mayonnaise@users.noreply.github.com>
2023-09-02 23:59:55 +0600

    Fixing others SQL

--- SQL/sources/1_creation/table_bankInfo.sql
+++ SQL/sources/1_creation/table_bankInfo.sql
@@ -1,8 +1,6 @@
 CREATE TABLE Bank_Info (
-    
-    bankId INTEGER PRIMARY KEY, -- constraint this has to be auto generated
-    routingNumber VARCHAR (20),
-    bankName VARCHAR (20),
-    branchName VARCHAR (20)
-    
+	bankId INTEGER PRIMARY KEY, -- constraint this has to be auto generated
+	routingNumber VARCHAR(20),
+	bankName VARCHAR(100),
+	branchName VARCHAR(100)
 );
