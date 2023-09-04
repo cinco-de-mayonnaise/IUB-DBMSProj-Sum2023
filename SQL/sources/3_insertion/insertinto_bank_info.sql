0b0020379c5d0c5363609aef44845d02492bf1c6
Mehedi <mehedinoorkhan16@gmail.com>
2023-09-04 19:20:09 +0600

    bank_info_sql

---
+++ SQL/sources/3_insertion/insertinto_bank_info.sql
@@ -0,0 +1,23 @@
+INSERT INTO `bank_info` (`bankId`, `routingNumber`, `bankName`, `branchName`)
+VALUES
+(1001, '123456780', 'Dhaka Bank', 'Motijheel Branch, Dhaka'),
+(1002, '987654321', 'Standard Chartered Bank Bangladesh', 'Gulshan Branch, Dhaka'),
+(1003, '111122233', 'Dutch-Bangla Bank', 'Dhanmondi Branch, Dhaka'),
+(1004, '444455566', 'BRAC Bank', 'Uttara Branch, Dhaka'),
+(1005, '777788899', 'Islami Bank Bangladesh', 'Chittagong Port Branch, Chittagong'),
+(1006, '222233344', 'United Commercial Bank', 'Cox's Bazar Branch, Cox's Bazar'),
+(1007, '555566677', 'Sonali Bank', 'Sylhet City Branch, Sylhet'),
+(1008, '999988877', 'Rupali Bank', 'Khulna Main Branch, Khulna'),
+(1009, '333322211', 'National Bank Limited', 'Rajshahi Central Branch, Rajshahi'),
+(1010, '666655544', 'Eastern Bank Limited', 'Barisal Divisional Branch, Barisal'),
+(1011, '888899977', 'Prime Bank Limited', 'Comilla Town Branch, Comilla'),
+(1012, '222211133', 'One Bank Limited', 'Mymensingh Campus Branch, Mymensingh'),
+(1013, '111133355', 'Trust Bank Limited', 'Rangpur District Branch, Rangpur'),
+(1014, '444466688', 'Mercantile Bank Limited', 'Narayanganj Riverside Branch, Narayanganj'),
+(1015, '555544433', 'Pubali Bank Limited', 'Bogura Commercial Branch, Bogura'),
+(1016, '777766655', 'City Bank Limited', 'Jessore City Branch, Jessore'),
+(1017, '111199988', 'AB Bank Limited', 'Tangail Branch, Tangail'),
+(1018, '888877766', 'Janata Bank Limited', 'Faridpur Town Branch, Faridpur'),
+(1019, '333322211', 'Agrani Bank Limited', 'Satkhira Coastal Branch, Satkhira'),
+(1020, '666655544', 'NRB Commercial Bank Limited', 'Pabna District Branch, Pabna')
+;
