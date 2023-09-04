2ea6bc8af9ef3e6b9dcf9f05f315341140cac4f7
joysaha007 <126173800+joysaha007@users.noreply.github.com>
2023-09-04 19:05:40 +0600

    Create insertinto_bo_account.sql

---
+++ SQL/sources/3_insertion/insertinto_bo_account.sql
@@ -0,0 +1,7 @@
+INSERT INTO BO_Account (BOapplicationNo, boCategory, boType, accOpeningDate, customerId, bankId, statementCycleType)
+VALUES
+    (1001, 'Savings', 'Personal', '2023-01-15', 1001, 101, 'Monthly'),
+    (1002, 'Current', 'Business', '2023-02-20', 1002, 102, 'Quarterly'),
+    (1003, 'Savings', 'Joint', '2023-03-10', 1003, 103, 'Monthly'),
+    (1004, 'Fixed Deposit', 'Personal', '2023-04-05', 1004, 104, 'Annually'),
+    (1005, 'Current', 'Business', '2023-05-12', 1005, 105, 'Monthly');
